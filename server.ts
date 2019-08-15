a7cf2dbe526133b76b8d6620bfb70929c5f8d052
lipeng <lipeng@daokoudai.com>
2019-08-15 09:51:45 +0800

    3.Node+Ts实战接口构建-接口调试工具postman

--- server.ts
+++ server.ts
@@ -8,6 +8,13 @@ app.get('/',(req,res,next)=>{
   res.send('node typescript api is working...')
 })
 
+app.get('/tours',(req,res,next)=>{
+  res.send('get tours...')
+})
+app.post('/tours',(req,res,next)=>{
+  res.send('post a new tours...')
+})
+
 app.listen(process.env.PORT || 8091,()=>{
   console.log('Server started...')
 })
