af9d0215d1ef384ddc71aba19eb502db2da77f61
lipeng <lipeng@daokoudai.com>
2019-08-15 12:53:30 +0800

    5.Node+Ts实战接口构建-优化get请求和获取单个数据

---
+++ model/shared/postSummary.ts
@@ -0,0 +1,12 @@
+export class PostSummary{
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  constructor(data:any){
+    this.userId = data.userId;
+    this.id = data.id;
+    this.title = data.title;
+    this.body = data.body;
+  }
+}
--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 // console.log('node-typescript-api');
 
 import express from 'express';
-import { DataStore } from './data/data';
+import { apiGetPosts,apiGetPostsDetail } from "./api/posts/apiGetPosts";
 const app = express();
 
 // console.log(JSON.parse(JSON.stringify(DataStore.posts)))
@@ -11,9 +11,8 @@ app.get('/',(req,res,next)=>{
   res.send('node typescript api is working...')
 })
 
-app.get('/posts',(req,res,next)=>{
-  res.json(DataStore.posts);
-})
+app.get('/posts',apiGetPosts)
+app.get('/posts/:id',apiGetPostsDetail)
 app.get('/tours',(req,res,next)=>{
   res.send('get tours...')
 })
