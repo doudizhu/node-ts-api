ff937f0e313c5282c9b030deb70f984fda7dd68e
lipeng <lipeng@daokoudai.com>
2019-08-16 16:31:10 +0800

    13.Node+Ts实战接口构建-剖析Request对象

--- server.ts
+++ server.ts
@@ -10,6 +10,7 @@ import { apiDeleteDetail } from "./api/posts/apiDeleteDetail";
 import { apiUpdateDetail } from "./api/posts/apiUpdateDetail";
 import { apiUploadImage } from "./api/posts/apiUploadImage";
 import { aipErrorHandler } from './api/general/errorHanding';
+import { APIError } from './model/shared/message';
 // import { CustomRequestHandler } from "./interface/express"; // 中间件
 const app = express();
 
@@ -62,6 +63,44 @@ app.post('/posts/:id/img',apiUploadImage)
 
 // 处理错误信息
 app.use(aipErrorHandler)
+
+// GET http://localhost:8091/posts/id2/todeos?star=5
+
+/**
+ * GET : req.method 
+ * http : req.protocol
+ * lhost : req.hostname
+ * port : environment
+ * posts/id2/todos : req.originalURL
+ * id2 : req.params = {postID: id2}
+ * ?star=5 : req.query={star:5}
+ * req.app // 获取生产环境
+ * req.body
+ * req.header
+ * req.app
+ * req.body
+ * req.secure,req.cookies,req.fresh...
+*/
+
+
+app.use((req,res,next)=>{
+  if(req.accepts('application/json')){
+    next()
+  }else{
+    next(new APIError(
+      'Content Type Not supported',
+      'This API only supports application/json',
+      404
+    ))
+  }
+})
+app.get('/headers',(req,res,next)=>{
+  res.json(req.headers)
+})
+app.post('/headers',(req,res,next)=>{
+  res.json(req.headers)
+})
+
 
 app.listen(process.env.PORT || 8091,()=>{
   console.log('Server started...')
